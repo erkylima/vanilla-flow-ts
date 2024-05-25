4c8a9bea827b811dc4a30ac145057b06a53ded19
Érky Lima <erkylima@gmail.com>
2024-05-25 11:54:04 -0300

    added new node to test

--- fine-tunning/src/app.ts
+++ fine-tunning/src/app.ts
@@ -24,11 +24,21 @@ export class App extends HTMLElement {
             x: 400,
             y: 160
         });
+        let node3 = new NodeComponent({
+            inputs: 1,
+            outputs: 2,
+            x: 600,
+            y: 160
+        });
         
         this.shadowRoot.appendChild(node1);
         this.shadowRoot.appendChild(node2);
+        this.shadowRoot.appendChild(node3);
         let edge = new EdgeComponent({startNode: node1, endNode: node2});
+        let edge2 = new EdgeComponent({startNode: node2, endNode: node3});
+
         this.shadowRoot.appendChild(edge);
+        this.shadowRoot.appendChild(edge2);
         const yourWebComponent = document.querySelector('node-component');
 
 
